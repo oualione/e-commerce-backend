2b5e55636212ebab1d1042be02e54db8d94e45d8
Khalid Ouali <ouali.linux@gmail.com>
2024-05-15 16:43:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 //app.use(cors())
 app.use(cors(
     {
-        origin: ['https://e-commerce-backend-tan-ten.vercel.app'],
+        origin: ['mern-react-puce.vercel.app'],
         methods : ['GET', 'POST'],
         credentials : true,
     }
