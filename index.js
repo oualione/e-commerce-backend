cde716ed31d1a1f9f4ad4865732390b158cf97b5
Khalid Ouali <ouali.linux@gmail.com>
2024-05-15 14:36:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const categoryRoutes = require('./routes/categoryRoutes')
 const productRoutes = require('./routes/productRoutes')
 const braintreeRoutes = require('./routes/braintreeRoutes')
 //Database Configuration
-mongoose.connect(process.env.DATABASE).then(() => {
+mongoose.connect('mongodb+srv://oualione:Ouali-1995@cluster0.tzggodf.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0').then(() => {
     console.log('Connected to Database Successfully ..')
 }).catch((error) => {
     console.log('Coonection failed ..' + error.message)
