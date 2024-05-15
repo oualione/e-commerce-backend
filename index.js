aa84bf394a6917811433006ed9097fa6f2dfd32c
Khalid Ouali <ouali.linux@gmail.com>
2024-05-15 19:17:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,30 @@ app.use(express.json())
 app.use(expressValidator())
 app.use(cookieParser())
 //app.use(cors())
-app.use(cors(
-    {
-        origin : ['https://mern-react-puce.vercel.app/login'],
-        methods : ['POST', 'GET'],
-        credentials : true
-    }
-));
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
+app.use(cors();
 
 require('dotenv').config()
 //Routes Declaration
