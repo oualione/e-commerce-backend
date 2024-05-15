eeffccf8852198b36bf38d81fcbf775559563c3f
Khalid Ouali <ouali.linux@gmail.com>
2024-05-15 19:53:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,30 +9,8 @@ const app = express()
 app.use(express.json())
 app.use(expressValidator())
 app.use(cookieParser())
-//app.use(cors())
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+app.use(cors())
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
 //app.use(cors());
 
 require('dotenv').config()
